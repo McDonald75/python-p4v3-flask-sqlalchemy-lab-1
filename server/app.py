3e5c596f9f93dc5775dbb5275cb6a80801ea98d6
McDonald75 <mcdonald.omondi@student.moringaschool.com>
2025-01-21 13:19:08 +0000

    complete

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +21,20 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def getEarthquake(id):
+    res = Earthquake.query.filter_by(id=id).first()
+    if res:
+        return make_response(jsonify(res.to_dict()), 200)
+    return make_response({'message':f'Earthquake {id} not found.'}, 404)
 
-
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def getEarthQueakeByMagnitude(magnitude):
+    res = Earthquake.query.filter(Earthquake.magnitude>=magnitude).all()
+    r = {
+        'count':len(res),
+        'quakes': [q.to_dict() for q in res]
+    }
+    return make_response(jsonify(r), 200)
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5556, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,12 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model,SerializerMixin):
+    __tablename__ = 'earthquakes'
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.Integer)
+    year = db.Column(db.Integer)
+
+    def __rep__(self):
+        print(f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>")
